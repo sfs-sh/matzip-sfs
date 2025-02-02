f880ceb545b355bd8ab50eb0928b8ef4a58db234
msHO <7fingersstudio@gmail.com>
2025-02-02 15:25:15 +0900

    modify axios

--- front/src/api/axios.ts
+++ front/src/api/axios.ts
@@ -3,11 +3,7 @@ import {Platform} from 'react-native';
 
 console.log(Platform.OS)
 
-const axiosInstance = axios.create({
-  // baseURL:
-  //   Platform.OS === 'android'
-  //     ? 'http://10.0.2.2:3030'
-  //     : 'http://localhost:3030',  
+const axiosInstance = axios.create({  
   baseURL: 'http://3.141.200.250:3030',
   withCredentials: true,
 });
